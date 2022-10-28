e90d76a4987eff6d41579d45a14f0f234e0bdef1
eryvonne <er_yvonne@yahoo.com.sg>
2022-10-28 06:48:42 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,15 @@
+module "s3_bucket" {
+  #source = "terraform-aws-modules/eryvonne/terraform-aws-s3-bucket"
+  
+  source  = "app.terraform.io/hashicat-aws-workshop/s3-bucket/aws"
+  version = "2.8.0"
+
+  #bucket = "terraform-aws-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+    bucket_prefix = prefix
+}
